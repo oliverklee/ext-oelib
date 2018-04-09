c11496c84ad151282c1d46a4289179e6c8eb1103
Oliver Klee <typo3-coding@oliverklee.de>
2018-04-09 18:53:15 +0200

    [TASK] Update Emogrifier to 20.0.0 and move it to Resources/Private/Php

--- Resources/Private/Php/vendor/composer/autoload_real.php
+++ Resources/Private/Php/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit3dba5e328e64d1b061a0ddb7f3872963
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit3dba5e328e64d1b061a0ddb7f3872963', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
