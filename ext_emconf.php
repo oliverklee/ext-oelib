5ce5e143cf901cc7deb1981a615d41cf8e3a2f56
Oliver Klee <typo3-coding@oliverklee.de>
2018-10-31 16:27:50 +0100

    [BUGFIX] Restore compatibility with PHP 5.4
    Also require PHP >= 5.4 in the ext_emconf.php.

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.6.99',
+			'php' => '5.4.0-5.6.99',
 			'typo3' => '4.5.0-6.2.99',
 			'static_info_tables' => '2.0.8-',
 		),
