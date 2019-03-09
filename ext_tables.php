17d09114665918fbdbdd9dcb6a8e7c1216adb978
Oliver Klee <typo3-coding@oliverklee.de>
2019-03-09 20:04:12 +0100

    [FEATURE] Static data geo coordinates of German ZIP codes
    This is the backport of #166 for #152.

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,23 @@ defined('TYPO3_MODE') or die('Access denied.');
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'One is Enough Library');
 
+$GLOBALS['TCA']['tx_oelib_domain_model_germanzipcode'] = array(
+    'ctrl' => array(
+        'default_sortby' => 'zip_code',
+        'delete' => 'deleted',
+        'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/ZipCode.gif',
+        'is_static' => true,
+        'label' => 'zip_code',
+        'label_alt' => 'city_name',
+        'label_alt_force' => true,
+        'readOnly' => true,
+        'rootLevel' => 1,
+        'searchFields' => 'zip_code, city_name',
+        'title' => 'LLL:EXT:oelib/Resources/Private/Language/locallang_db.xml:tx_oelib_domain_model_germanzipcode',
+        'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/GermanZipCode.php',
+    ),
+);
+
 $GLOBALS['TCA']['tx_oelib_test'] = array(
 	'ctrl' => array(
 		'title' => 'LLL:EXT:oelib/Resources/Private/Language/locallang_db.xml:tx_oelib_test',
