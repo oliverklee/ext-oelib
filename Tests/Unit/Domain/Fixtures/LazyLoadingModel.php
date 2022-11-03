af2bcb7fc6d7c14aa1e970e8f3ffba358b58f98d
Braunson Yager <Braunson@users.noreply.github.com>
2022-11-03 16:07:05 -0400

    Reverted changes

--- Tests/Unit/Domain/Fixtures/LazyLoadingModel.php
+++ Tests/Unit/Domain/Fixtures/LazyLoadingModel.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OliverKlee\Oelib\Tests\Unit\Domain\Fixtures;
 
+use OliverKlee\Oelib\Domain\Model\Traits\LazyLoadingProperties;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
 
@@ -12,6 +13,8 @@ use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
  */
 final class LazyLoadingModel extends AbstractEntity
 {
+    use LazyLoadingProperties;
+
     /**
      * @var EmptyModel
      * @phpstan-var EmptyModel|LazyLoadingProxy
@@ -20,13 +23,7 @@ final class LazyLoadingModel extends AbstractEntity
 
     public function getLazyProperty(): EmptyModel
     {
-        // @phpstan-ignore-next-line This variable property access is okay.
-        $propertyValue = $this->lazyProperty;
-        if ($propertyValue instanceof LazyLoadingProxy) {
-            // @phpstan-ignore-next-line This variable property access is okay.
-            $this->lazyProperty = $propertyValue->_loadRealInstance();
-        }
-        
+        $this->loadLazyProperty('lazyProperty');
         /** @var EmptyModel $property */
         $property = $this->lazyProperty;
 
