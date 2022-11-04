d13915429741bcf4968bc49baf492da5360b2bca
Oliver Klee <github@oliverklee.de>
2022-11-04 11:23:03 +0100

    Remove trailing whitespace

--- Classes/Domain/Model/Traits/LazyLoadingProperties.php
+++ Classes/Domain/Model/Traits/LazyLoadingProperties.php
@@ -11,7 +11,7 @@ use TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy;
  * This provides a method for loading lazy n:1 and 1:1 properties.
  *
  * @deprecated Will be removed in oelib 6.0.
- *  
+ *
  * @mixin AbstractEntity
  */
 trait LazyLoadingProperties
