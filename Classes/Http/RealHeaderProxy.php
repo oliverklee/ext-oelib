34599dbfecfd34af223d54cd4a17bd7c1047d9fd
Oliver Klee <typo3-coding@oliverklee.de>
2023-12-21 01:11:19 +0100

    [TASK] Deprecate the HTTP classes
    Fixes #1445

--- Classes/Http/RealHeaderProxy.php
+++ Classes/Http/RealHeaderProxy.php
@@ -11,6 +11,8 @@ use TYPO3\CMS\Core\Utility\HttpUtility;
  * This class sends HTTP headers.
  *
  * Regarding the Strategy pattern, `addHeader()` represents one concrete behavior.
+ *
+ * @deprecated #1526 will be removed in oelib 6.0.0
  */
 class RealHeaderProxy implements HeaderProxy
 {
